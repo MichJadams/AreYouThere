51c98f4e71fe6489dd84943258794474d223887b
michjadams <Mjadams66@gmail.com>
2018-06-07 10:59:33 -0400

    collision detection works

--- server.js
+++ server.js
@@ -188,50 +188,73 @@ function randomLocation (max){
 }
 function movement(keycode,playerlocation){
     //check for collision?? somehow....
-    // console.log("player location", playerlocation)
+    // console.log("player location", playerlocation),it needs to look ahead, collision detection after the augmentation fo movement. 
     const mapOne = require('./mapOne')
-    const collisionCheckPlayerLocation = playerlocation.x.toString().concat(playerlocation.y,playerlocation.z)
-    console.log("looking for ",collisionCheckPlayerLocation, "inside of map one collision hash:",mapOne.mapTwoCollisionHash[+collisionCheckPlayerLocation])
+    // const collisionCheckPlayerLocation = playerlocation.x.toString().concat(playerlocation.y,playerlocation.z)
+    // console.log("looking for ",collisionCheckPlayerLocation, "inside of map one collision hash:",mapOne.mapTwoCollisionHash[+collisionCheckPlayerLocation])
     
-    if(mapOne.mapTwoCollisionHash[+collisionCheckPlayerLocation] === true){
-        console.log("moving allowed", mapOne.mapTwoCollisionHash[+collisionCheckPlayerLocation])
+    // if(mapOne.mapTwoCollisionHash[+collisionCheckPlayerLocation] === true){
+    //     console.log("moving allowed: this",collisionCheckPlayerLocation,"is equal to:", mapOne.mapTwoCollisionHash[+collisionCheckPlayerLocation])
     
 
     const deltMovement = 1 
-
         if(keycode == 87){
             //forward
             // console.log("old player location in terms of z", playerlocation.z)
             // console.log("forward, returning:",new THREE.Vector3(0,0,playerlocation.z+5))
-            return new THREE.Vector3(playerlocation.x,playerlocation.y,playerlocation.z-1)
+
+            const collisionCheckPlayerLocation = playerlocation.x.toString().concat(playerlocation.y,playerlocation.z-deltMovement)
+            if(mapOne.mapTwoCollisionHash[+collisionCheckPlayerLocation] === true){
+                // console.log("moving allowed: this",collisionCheckPlayerLocation,"is equal to:", mapOne.mapTwoCollisionHash[+collisionCheckPlayerLocation])
+                return new THREE.Vector3(playerlocation.x,playerlocation.y,playerlocation.z-1)
+            }
         }
         if(keycode == 65){
             //left
             // console.log("left")
-            return new THREE.Vector3(playerlocation.x-deltMovement,playerlocation.y,playerlocation.z)
+            const collisionCheckPlayerLocation = (playerlocation.x-deltMovement).toString().concat(playerlocation.y,playerlocation.z)
+            if(mapOne.mapTwoCollisionHash[+collisionCheckPlayerLocation] === true){
+                // console.log("moving allowed: this",collisionCheckPlayerLocation,"is equal to:", mapOne.mapTwoCollisionHash[+collisionCheckPlayerLocation])
+                return new THREE.Vector3(playerlocation.x-deltMovement,playerlocation.y,playerlocation.z)
+            }
         }
         if(keycode == 83){
             //backwards
             // console.log("backwards")
-            return new THREE.Vector3(playerlocation.x,playerlocation.y,playerlocation.z+deltMovement)
+            const collisionCheckPlayerLocation = playerlocation.x.toString().concat(playerlocation.y,playerlocation.z+deltMovement)
+            if(mapOne.mapTwoCollisionHash[+collisionCheckPlayerLocation] === true){
+                // console.log("moving allowed: this",collisionCheckPlayerLocation,"is equal to:", mapOne.mapTwoCollisionHash[+collisionCheckPlayerLocation])
+                return new THREE.Vector3(playerlocation.x,playerlocation.y,playerlocation.z+deltMovement)
+            }
         }
         if(keycode == 68){
             //right
             // console.log("right")
-            return new THREE.Vector3(playerlocation.x+deltMovement,playerlocation.y,playerlocation.z)
+            const collisionCheckPlayerLocation = (playerlocation.x+deltMovement).toString().concat(playerlocation.y,playerlocation.z)
+            if(mapOne.mapTwoCollisionHash[+collisionCheckPlayerLocation] === true){
+                // console.log("moving allowed: this",collisionCheckPlayerLocation,"is equal to:", mapOne.mapTwoCollisionHash[+collisionCheckPlayerLocation])
+                return new THREE.Vector3(playerlocation.x+deltMovement,playerlocation.y,playerlocation.z)
+            }
         }
         if(keycode == 69){
             //up
             // console.log("right")
-            return new THREE.Vector3(playerlocation.x,playerlocation.y+deltMovement,playerlocation.z)
+            const collisionCheckPlayerLocation = playerlocation.x.toString().concat(playerlocation.y+deltMovement,playerlocation.z)
+            if(mapOne.mapTwoCollisionHash[+collisionCheckPlayerLocation] === true){
+                // console.log("moving allowed: this",collisionCheckPlayerLocation,"is equal to:", mapOne.mapTwoCollisionHash[+collisionCheckPlayerLocation])
+                return new THREE.Vector3(playerlocation.x,playerlocation.y+deltMovement,playerlocation.z)
+            }
         }
         if(keycode == 81){
             //down
             // console.log("right")
-            return new THREE.Vector3(playerlocation.x,playerlocation.y-deltMovement,playerlocation.z)
+            const collisionCheckPlayerLocation = playerlocation.x.toString().concat(playerlocation.y-deltMovement,playerlocation.z)
+            if(mapOne.mapTwoCollisionHash[+collisionCheckPlayerLocation] === true){
+                // console.log("moving allowed: this",collisionCheckPlayerLocation,"is equal to:", mapOne.mapTwoCollisionHash[+collisionCheckPlayerLocation])
+                return new THREE.Vector3(playerlocation.x,playerlocation.y-deltMovement,playerlocation.z)
+            }
         }
-    }else{
-        console.log("false, the player location is ", playerlocation)
-        // return new THREE.Vector3(playerlocation.x,playerlocation.y,playerlocation.z)
-    }
+        console.log("movmenet not allowed")
+        return playerlocation
+    
 }
